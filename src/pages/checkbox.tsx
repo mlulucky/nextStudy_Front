a155f24ae90d069c05822be0c60b2aaf8f27beba
mlulucky <dmswjdans@gmail.com>
2023-07-26 18:10:43 +0900

    로그인화면 구현

---
+++ src/pages/checkbox.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import styled from "styled-components";
+import iconImg from "@public/icon.png";
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  padding-left: 23px;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-position: -244px -87px;
+    background-repeat: no-repeat;
+    width: 18px;
+    height: 18px;
+    background-color: #fff;
+    background-image: url(${iconImg.src});
+    background-size: 266px 225px;
+    background-repeat: no-repeat;
+  }
+`;
+const LoginKeepWrap = styled.div`
+  margin-top: 15px;
+  text-align: left;
+  position: relative;
+  label {
+    cursor: pointer;
+  }
+
+  ${Input}:checked + ${Label}::before {
+    background-position: -244px -167px;
+    background-repeat: no-repeat;
+    width: 18px;
+    height: 18px;
+  }
+`;
+
+export default function CheckBox() {
+  let [isChecked, setIsChecked] = useState(false);
+
+  const check = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <LoginKeepWrap>
+        <Input type="checkbox" checked={isChecked}/>
+        <Label
+            onClick={() => {
+            check();
+            }}
+        >  
+            로그인상태 유지
+        </Label>
+    </LoginKeepWrap>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import styled from 'styled-components'
+import CheckBox from './checkbox'
+import checkImg from '@public/check.png'
+import iconImg from '@public/icon.png'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +19,12 @@ export default function Home() {
     text-align: center;
   `
   const Form = styled.form`
-    border: 1px solid red;
+    border: 1px solid #c6c6c6;
     width: 400px;
     height: 300px;
-    padding: 10px;
+    padding: 20px 30px;
+    border-radius: 6px;
+
   `
 
   const Title = styled.h3`
@@ -26,28 +32,47 @@ export default function Home() {
   `
   const Button = styled.button`
     width: 100%;
+    border-radius: 6px;
+    border: solid 1px rgba(0,0,0,.15);
+    background-color: #03c75a;
+    box-sizing: border-box;
+    padding: 13px 0 13px;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+    color: #fff;
+    cursor: pointer;
+    margin-top: 40px;
   `
 
-  const Input = styled.input`
+  // styled-components 에 props 속성 주기
+  const Input = styled.input<{isFirst?:boolean}>`
     width: 100%;
     box-sizing: border-box;
     padding: 14px 17px 13px;
-    margin-bottom: 10px;
-    border-radius: 5px;
     border: 1px solid #dadada;
-    
+    margin-top: ${({isFirst}) => isFirst ? '0' : '-1px'};
+    border-radius: ${({isFirst}) => isFirst ? '6px 6px 0 0' : '0 0 6px 6px'};
+    position: relative;
+
     &:focus {
-      border: 1px solid red;
+      border: 1px solid #03c75a;
+      outline: none;
+      z-index: 5;
     }
   `
+
+
 
   return (
     <Div>
       <Form>
-        <Title>로그인</Title>
-        <div><Input placeholder='아이디'/></div>
-        <div><Input placeholder='비밀번호'/></div>
-        <input type="checkbox"/>로그인 상태 유지
+        <Title>Login</Title>
+        <div style={{boxShadow: '0 2px 6px 0 rgba(68,68,68,.08)'}}>
+          <Input isFirst placeholder='아이디'/>
+          <Input placeholder='비밀번호'/>
+        </div>
+        <CheckBox/>
         <Button>로그인</Button>
       </Form>
     </Div>
