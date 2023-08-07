14cd6148cf2efddd4d8889390744c0fcae6b7a73
mlulucky <dmswjdans@gmail.com>
2023-08-08 07:38:18 +0900

    할일 체크시 체크박스 디자인 수정

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -12,8 +12,7 @@ const Remove = styled.div`
     color: #ff6b6b;
   }
   visibility: hidden;
-
-`; 
+`;
 
 const List = styled.li`
 	padding: 10px 0;
@@ -24,35 +23,53 @@ const List = styled.li`
 			visibility: visible;
 `;
 
-const ToDo = styled.div<{ done: boolean }>` // 태그 props 타입
+const Div = styled.div<{done: boolean}>`
 	flex: 1;
-	text-align: left;
-  ${props =>
-    props.done && css`
-      text-decoration: line-through;
-      color: gray;
-    `}
-`
+	display: flex;
+	align-items: center;
+	${props => props.done && css`
+		${Check} {
+			border: 1px solid #03c75a;
+			background-color: #03c75a;
+			color: #fff;
+		}
+		${ToDo} {
+			text-decoration: line-through;
+			color: gray;
+		}
+	`}    
+`;
+
 const Check = styled.div`
 	width: 20px;
 	height: 20px;
 	border-radius: 50%;
 	border: 1px solid #ced4da;
+	color: #ced4da;
   font-size: 24px;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-right: 20px;
   cursor: pointer;
-`
+`;
+
+const ToDo = styled.div` 
+	flex: 1;
+	text-align: left;
+  display: flex;
+  align-items: center;
+`;
 
 // 할 일
 export default function ToDoItem({todo, toggleToDo, removeToDo}: {todo : ToDos, toggleToDo: toggleRmoveType['toggleToDo'], removeToDo: toggleRmoveType['removeToDo']}) { 
   return(
     <List>
-      <Check>  <MdDone/> </Check>
-      <ToDo done={todo.done} onClick={ ()=> {toggleToDo(todo.id)} }>{todo.todo}</ToDo>
-      <Remove className="remove" onClick={ () => { removeToDo(todo.id)} } > <MdRemoveCircleOutline /> </Remove>
+      <Div done={todo.done} onClick={()=>{toggleToDo(todo.id)}}>
+        <Check> <MdDone/> </Check>
+        <ToDo>{todo.todo}</ToDo>
+      </Div>
+      <Remove className="remove" onClick={()=>{removeToDo(todo.id)}} > <MdRemoveCircleOutline /> </Remove>
     </List>
 
   )
