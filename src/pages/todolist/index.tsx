c3fb8bd400bdd390e71941278cf13e29fec30d05
mlulucky <dmswjdans@gmail.com>
2023-08-18 17:51:55 +0900

    PageWrapper 컴포넌트 적용

--- src/pages/todolist/index.tsx
+++ src/pages/todolist/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import styled from "styled-components";
 import ToDoTemplate from "@/components/ToDoTemplate";
+import { PageWrapper } from "@/styles/PageWrapper";
 
-const Div = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  height: 100vh;
-`;
 
 export default function ToDoListPage() {
   return (
-    <Div>
+    <PageWrapper>
       <ToDoTemplate></ToDoTemplate>
-    </Div>
+    </PageWrapper>
   );
 }
