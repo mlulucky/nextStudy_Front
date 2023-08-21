c17e10d09439a8b0ff23ab2f438cf6cd69208f24
mlulucky <dmswjdans@gmail.com>
2023-08-21 19:04:50 +0900

    refactor: 회원가입 axios 함수 적용으로 리팩토링

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
-import axios from 'axios'
-import Box from '@mui/material/Box';
 import Form from '@/components/Form'
 import Button from '@/components/Button';
 import { PageWrapper } from '@/styles/PageWrapper';
+import { joinAPI } from '../api/auth';
 
 export default function Join(){
-    const [requestResult, setRequestResult] = useState<string>('');
     const [account, setAccount] = useState<string>('');
     const [userName, setUserName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [passwordCheck, setPasswordCheck] = useState<string>('');
 
-    const joinHandler = (e : React.MouseEvent<HTMLButtonElement>) => {
+    const joinHandler = async (e : React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         const data = {
             account,
@@ -23,26 +21,22 @@ export default function Join(){
             passwordCheck
         };
 
-        axios.post("http://localhost:8080/api/user/join", data) // axios.post(url, data) : post 방식으로 url 에 data 전달 - request body에 전달할 data(json)
-        .then((response)=>{
-            console.log(response);
-            setRequestResult('성공');
-        }).catch((error)=> {
-            setRequestResult('에러');
-            console.log(error);
-        })
-
-        if(!account) {
-            alert("계정을 입력해주세요");
+        const joinResponse = await joinAPI(data);
+        if(!joinResponse) {
+            alert('회원가입에 실패했습니다.');
+            return;
+        }
+        if(!joinResponse.result) {
+            alert('회원가입에 실패했습니다.');
+            return;
         }
 
+        alert("회원가입에 성공했습니다.");
+    
     }
 
     return (
 			<PageWrapper>
-				<Box >
-						{requestResult}
-				</Box>
 				<Form>
 					<h3>회원가입</h3>
 					<Form.Input placeholder="계정" onChange={(e)=>{setAccount(e)}}/>
