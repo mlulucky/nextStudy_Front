62d128dcd7cd3496aae17c78b487d01af0eddffd
mlulucky <dmswjdans@gmail.com>
2023-08-21 19:07:15 +0900

    feat: NavWrapper stylecomponent 임시 추가

--- src/styles/PageWrapper.ts
+++ src/styles/PageWrapper.ts
@@ -6,4 +6,13 @@ export const PageWrapper = styled.div`
   align-items: center;
   text-align: center;
   height: 100vh;
+  padding: 1rem;
+`
+
+export const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  box-shadow: 0px 0px 4px 0px #c6c6c6;
 `
