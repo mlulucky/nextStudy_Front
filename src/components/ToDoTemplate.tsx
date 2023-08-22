2716aba98351110d06e7dc93cf7999dd1f7ffc8f
mlulucky <dmswjdans@gmail.com>
2023-08-22 18:00:07 +0900

    feat: 투두리스트 요청 axios 작업중

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import styled from "styled-components";
-import ToDoInsert from "./TodoInsert";
+import ToDoInsert from "./ToDoInsert";
 import ToDoList from "./ToDoList";
 import { useToDo } from "@/hooks/useToDo";
 import ToDoHead from "./ToDoHead";
+import { addToDoAPI } from "@/pages/api/todo";
+import userStore from "@/store/userStore";
+import axios from "axios";
+import { responsiveFontSizes } from "@mui/material";
+import { useCookies } from "react-cookie";
 
 const ToDoWrap = styled.div`
   border: 1px solid #c6c6c6;
@@ -18,6 +23,54 @@ const ToDoWrap = styled.div`
 
 export default function ToDoTemplate() {
   const {state, addToDo, toggleToDo, removeToDo, updateToDo} = useToDo();
+  
+
+  const {user, setUser} = userStore();
+  const [userId, setUserId] = useState<number>();
+  const [content, setContent] = useState<string>();
+  const [done, setDone] = useState<boolean>();
+  const [cookies, setCookies] = useCookies();
+
+  // id
+  // userId
+  // content
+  // done
+
+  const [todoList, setTodoList] = useState([]); // 👀 todoList 타입 제네릭 설정하기
+
+  const todoCreateHandler = async () => {
+    const data = {
+      // account : 
+      userId,
+      content,
+      done
+    }
+    
+    const toDoResponse = await addToDoAPI(data);
+
+
+  }
+
+  const getToDoList = async (token: string) => {
+    const requestToken = {
+      headers: {
+        Authorization: `Bearer ${token}` // 쿠키에서 토큰을 가져와서 헤더에 포함
+      }
+    }
+
+    await axios.get(`/api/todo/${user.id}/list`, requestToken)
+    .then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log("getToDo Error", error);
+    })
+  }
+
+  useEffect(()=>{
+    const token = cookies.token;
+    if(token) getToDoList(token);
+  },[]);
+
 
   return (
     <ToDoWrap>
@@ -25,7 +78,6 @@ export default function ToDoTemplate() {
       <ToDoInsert addToDo={addToDo}/>
       <ToDoList state={state} toggleToDo={toggleToDo} removeToDo={removeToDo} updateToDo={updateToDo}/>
     </ToDoWrap>
-
   );
 
 }
