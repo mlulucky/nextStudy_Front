04942bd9ee537c1591ae822db9b2314e4f267fc2
mlulucky <dmswjdans@gmail.com>
2023-08-22 17:58:00 +0900

    feat: api 요청 proxy 설정 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  async rewrites() { // api 요청 proxy 설정 (cors 우회처리)
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:8080/:path*"
+      },
+    ]
+  },
+  
 }
 
 module.exports = nextConfig
