76993d8ebc4e9810a310045f0734a2a7e731d2d6
mlulucky <dmswjdans@gmail.com>
2023-08-23 23:49:40 +0900

    fix: 응답 response 콘솔 출력

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -60,7 +60,7 @@ export default function ToDoTemplate() {
 
     await axios.get(`/api/todo/${user.id}/list`, requestToken)
     .then((response) => {
-      console.log(response.data);
+      console.log(response);
     }).catch((error) => {
       console.log("getToDo Error", error);
     })
