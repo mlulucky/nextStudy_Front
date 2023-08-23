0b54da01f5d486be7c1b44a0ef863b43595090fd
mlulucky <dmswjdans@gmail.com>
2023-08-23 17:44:14 +0900

    feat: 로그인,회원가입 userRequest DTO 생성

---
+++ src/dto/UserJoinRequestDTO.ts
@@ -0,0 +1,9 @@
+type UserJoinRequestDTO = {
+    account: string;
+    userName: string;
+    email: string;
+    password: string;
+    passwordCheck: string;
+}
+
+export default UserJoinRequestDTO;
