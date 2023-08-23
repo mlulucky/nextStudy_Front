fd8aa8a3cc93e1283d3f53c65ad9b8abe50208de
mlulucky <dmswjdans@gmail.com>
2023-08-24 07:53:15 +0900

    refactor: onChange 함수 및 코드 정리

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -12,16 +12,15 @@ export default function Join(){
     const [password, setPassword] = useState<string>('');
     const [passwordCheck, setPasswordCheck] = useState<string>('');
     const { userJoin } = useAuth();
-
+    const data = {
+        account, // account : account // setAccount() 사용하여 입력한 값을 account 로 저장 -> api 로 서버에 data 전달예정
+        userName,
+        email,
+        password,
+        passwordCheck
+    };
     const joinHandler = async (e : React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        const data = {
-            account, // account : account // setAccount() 사용하여 입력한 값을 account 로 저장 -> api 로 서버에 data 전달예정
-            userName,
-            email,
-            password,
-            passwordCheck
-        };
         userJoin(data); // 회원가입 hook(joinAPI + 회원가입로직)
     }
 
@@ -29,12 +28,12 @@ export default function Join(){
 			<PageWrapper>
 				<Form>
 					<h3>회원가입</h3>
-					<Form.Input placeholder="계정" onChange={(e)=>{setAccount(e)}}/>
-					<Form.Input placeholder="이름" onChange={(e)=>{setUserName(e)}}/>
-					<Form.Input placeholder="이메일" onChange={(e)=>{setEmail(e)}}/>
-					<Form.Input placeholder="비밀번호" onChange={(e)=>{setPassword(e)}}/>
-					<Form.Input placeholder="비밀번호확인" onChange={(e)=>{setPasswordCheck(e)}}/>
-					<Button onClick={(e)=>{ joinHandler(e); }}>가입하기</Button>
+					<Form.Input placeholder="계정" onChange={setAccount}/>
+					<Form.Input placeholder="이름" onChange={setUserName}/>
+					<Form.Input placeholder="이메일" onChange={setEmail}/>
+					<Form.Input placeholder="비밀번호" onChange={setPassword}/>
+					<Form.Input placeholder="비밀번호확인" onChange={setPasswordCheck}/>
+					<Button onClick={joinHandler}>가입하기</Button>
 				</Form>
 			</PageWrapper>
     )
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,19 +10,13 @@ export function Login(){
   const [password, setPassword] = useState<string>("");
   const { userLogin } = useAuth(); // 함수에 반환값 타입을 설정했으므로 타입스크립트가 반환된 타입으로 타입추론예정(제네릭x)
 
+  const data = {
+    account,  // account : account // key 와 value 의 이름이 같은 경우 값을 하나만 적어도 적용된다. // request dto 의 필드명과 이름이 같아야 한다.
+    password  
+  }
+
   const loginHandler = async (e: React.MouseEvent<HTMLButtonElement>) => { // e : 여기서는 onClick 클릭이벤트 타입
     e.preventDefault(); // 페이지 새로고침 방지
-
-    if(account.length === 0 || password.length === 0){ // 🌈 아이디, 비밀번호 검증 분리하기!
-      alert("이메일과 비밀번호를 입력하세요.");
-      return; // loginHandler 함수 종료
-    }
-    
-    const data = {
-      account,  // account : account // key 와 value 의 이름이 같은 경우 값을 하나만 적어도 적용된다. // request dto 의 필드명과 이름이 같아야 한다.
-      password  
-    }
-
     userLogin(data); // 로그인 hook(loginAPI + 로그인로직)   
   }
 
@@ -33,11 +27,11 @@ export function Login(){
         <Form>
             <h3>Login</h3>
             <div style={{boxShadow: '0 2px 6px 0 rgba(68,68,68,.08)'}}>
-            <Form.Input isfirst placeholder='아이디'  onChange={(e)=>{setAccount(e)}}/>
-            <Form.Input placeholder='비밀번호'  onChange={(e)=>{setPassword(e)}}/>
+            <Form.Input isfirst placeholder='아이디'  onChange={setAccount}/>
+            <Form.Input placeholder='비밀번호'  onChange={setPassword}/>
             </div>
             <CheckBox/>
-            <Button onClick={(e)=>{ loginHandler(e); }}>로그인</Button>
+            <Button onClick={loginHandler}>로그인</Button>
         </Form>
         <div style={{color: '#777'}}>아직 회원이 아니신가요? 회원가입</div>
       </div>  
