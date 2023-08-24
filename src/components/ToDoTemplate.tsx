52862400d461f073fe6e3a8d0844eeb67eca9558
mlulucky <dmswjdans@gmail.com>
2023-08-24 17:54:13 +0900

    refactor: Form 컴포넌트 적용 테스트중

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ToDoInsert from "./ToDoInsert";
 import ToDoList from "./ToDoList";
@@ -6,6 +6,8 @@ import { useToDo } from "@/hooks/reducer/useToDo";
 import useToDoService from "@/hooks/useToDoService";
 import ToDoHead from "./ToDoHead";
 import todoStore from "@/store/todoStore";
+import Form from "./Form";
+import userStore from "@/store/userStore";
 
 const ToDoWrap = styled.div`
   border: 1px solid #c6c6c6;
@@ -19,13 +21,21 @@ const ToDoWrap = styled.div`
 
 export default function ToDoTemplate() {
   const {state, addToDo, toggleToDo, removeToDo, updateToDo} = useToDo();
-  const {getToDos} = useToDoService();
+  const {getToDos, addToDoService} = useToDoService();
   const {todos} = todoStore();
+  const [value, setValue] = useState('');
+  const {user} = userStore();
+  
 
   useEffect(()=>{
     getToDos();
-  },[])
+  },[]);
 
+  const data = {
+    userId: user.id,
+    //content: string; // 입력한 값
+    done: false // 기본 값
+  }
 
   return (
     <>
@@ -36,11 +46,16 @@ export default function ToDoTemplate() {
           );
         })
       }
-      <ToDoWrap>
+      <Form style={{height: '500px'}}>
+        <ToDoHead state={state}/>      
+        {/* <Form.Input addToDo={addToDoService(data)}/> */}
+        <ToDoList state={state} toggleToDo={toggleToDo} removeToDo={removeToDo} updateToDo={updateToDo}/>
+      </Form>
+      {/* <ToDoWrap>
         <ToDoHead state={state}/>      
         <ToDoInsert addToDo={addToDo}/>
         <ToDoList state={state} toggleToDo={toggleToDo} removeToDo={removeToDo} updateToDo={updateToDo}/>
-      </ToDoWrap>
+      </ToDoWrap> */}
     </>
   );
 
--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -1,5 +1,9 @@
 import React, { FormEvent, useState, SetStateAction, Dispatch} from 'react'
 import styled from "styled-components";
+import Form from './Form';
+import CheckBox from './CheckBox';
+import Button from './Button';
+import useAuth from '@/hooks/useAuth';
 
 const Input = styled.input`
   padding: 12px;
@@ -22,7 +26,20 @@ const Input = styled.input`
 // 할 일 등록 컴포넌트
 export default function ToDoInsert({addToDo} : {addToDo: (text: string) => void }) {
     const [value, setValue] = useState('');
-    
+    const [account, setAccount] = useState<string>(""); // useState 안에있는 배열의 요소 순서대로 account, setAccount 변수에 대입
+    const [password, setPassword] = useState<string>("");
+    const { userLogin } = useAuth(); // 함수에 반환값 타입을 설정했으므로 타입스크립트가 반환된 타입으로 타입추론예정(제네릭x)
+
+    const data = {
+      account,  // account : account // key 와 value 의 이름이 같은 경우 값을 하나만 적어도 적용된다. // request dto 의 필드명과 이름이 같아야 한다.
+      password  
+    }
+    const loginHandler = async (e: React.MouseEvent<HTMLButtonElement>) => { // e : 여기서는 onClick 클릭이벤트 타입
+      e.preventDefault(); // 페이지 새로고침 방지
+      userLogin(data); // 로그인 hook(loginAPI + 로그인로직)   
+    }
+
+
     const onSubmit = (e: FormEvent) => {
 			e.preventDefault();
 			if(value.trim() == "") { alert("할 일을 입력해주세요.") }  
@@ -33,7 +50,18 @@ export default function ToDoInsert({addToDo} : {addToDo: (text: string) => void 
 		}
 
     return (
-        <form onSubmit={ onSubmit } style={{flex: "1"}}>
+      <>
+        <Form onSubmit={ onSubmit } style={{flex: "1"}}>
+					<Form.Input
+            onChange={setValue}
+            value={value}
+            placeholder="할 일을 입력하고, Enter를 치세요"
+          />
+          <button style={{ display: "none" }} />
+        </Form>
+
+
+        {/* <form onSubmit={ onSubmit } style={{flex: "1"}}>
 					<Input
             autoFocus
             onChange={(e) => { setValue(e.target.value); }}
@@ -41,6 +69,9 @@ export default function ToDoInsert({addToDo} : {addToDo: (text: string) => void 
             placeholder="할 일을 입력하고, Enter를 치세요"
           />
           <button style={{ display: "none" }} />
-        </form>
+        </form> */}
+      
+      </>
+      
     );
 }
