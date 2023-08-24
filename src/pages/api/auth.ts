4e17ba7f6663d3be857cb6b2d9c378762ecd2f50
mlulucky <dmswjdans@gmail.com>
2023-08-24 15:05:04 +0900

    refactor: if문 수정 및 return 간략하게 수정

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -7,10 +7,8 @@ const authRequest = async (url:string, data: any) => {
     // await axios : axios 로 데이터를 받아온 이후 데이터 로직 처리
     // axios.post(url, data) : post 방식으로 url 에 data 전달 - request body에 전달할 data(json)
     const response = await axios.post(url, data).catch((error) => {console.log(error); null});  // .catch((error) => null); 에러발생시 중단없이 계속진행할때(에러 무시)
-    if(!response) return null; // response 가 없으면(null 이면_ axios 에서 catch(error) 일때) null 반환 
-
-    const result = response.data;
-    return result;
+    if(!response) {alert("응답데이터가 없습니다."); return null;}
+    return response.data;
 }
 
 
