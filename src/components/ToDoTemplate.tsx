a6630c7c425e772a04411baa62e3d65280eae907
mlulucky <dmswjdans@gmail.com>
2023-08-25 11:50:19 +0900

    refactor: 코드정리

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -6,8 +6,6 @@ import { useToDo } from "@/hooks/reducer/useToDo";
 import useToDoService from "@/hooks/useToDoService";
 import ToDoHead from "./ToDoHead";
 import todoStore from "@/store/todoStore";
-import Form from "./Form";
-import userStore from "@/store/userStore";
 
 const ToDoWrap = styled.div`
   border: 1px solid #c6c6c6;
@@ -23,30 +21,10 @@ export default function ToDoTemplate() {
   const {state, addToDo, toggleToDo, removeToDo, updateToDo} = useToDo();
   const {getToDos, addToDoService} = useToDoService();
   const {todos} = todoStore();
-  const [value, setValue] = useState('');
-  const {user} = userStore();
   
-
   useEffect(()=>{
     getToDos();
   },[]);
-
-  const data = {
-    userId: user.id,
-    content: value, // 입력한 값
-    done: false // 기본 값
-  }
-
-  const 할일등록 = (e: FormEvent) => {
-    e.preventDefault();
-    if(value.trim() == "") { alert("할 일을 입력해주세요.") } 
-    else {
-      addToDoService(data);
-      setValue(''); // 입력값 초기화
-    }
-  
-  
-  }
 
   return (
     <>
@@ -57,12 +35,6 @@ export default function ToDoTemplate() {
           );
         })
       }
-      {/* <Form onSubmit={할일등록} style={{height: '500px'}}>
-        <ToDoHead state={state}/>      
-        <Form.Input placeholder="할 일을 입력하고, Enter를 치세요" onChange={setValue} />
-        <ToDoList state={state} toggleToDo={toggleToDo} removeToDo={removeToDo} updateToDo={updateToDo}/>
-        <button style={{ display: "none" }} />
-      </Form> */}
       <ToDoWrap>
         <ToDoHead state={state}/>      
         <ToDoInsert addToDo={addToDo}/>
--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -4,6 +4,9 @@ import Form from './Form';
 import CheckBox from './CheckBox';
 import Button from './Button';
 import useAuth from '@/hooks/useAuth';
+import useToDoService from '@/hooks/useToDoService';
+import userStore from '@/store/userStore';
+import ToDoCreateRequestDTO from '@/dto/ToDoCreateRequestDTO';
 
 const Input = styled.input`
   padding: 12px;
@@ -21,37 +24,30 @@ const Input = styled.input`
   }
 `;
 
-// React.Dispatch<React.SetStateAction<state의 타입>>; - setValue 는 Dispatch 타입
-
 // 할 일 등록 컴포넌트
 export default function ToDoInsert({addToDo} : {addToDo: (text: string) => void }) {
     const [value, setValue] = useState('');
-    const [account, setAccount] = useState<string>(""); // useState 안에있는 배열의 요소 순서대로 account, setAccount 변수에 대입
-    const [password, setPassword] = useState<string>("");
-    const { userLogin } = useAuth(); // 함수에 반환값 타입을 설정했으므로 타입스크립트가 반환된 타입으로 타입추론예정(제네릭x)
+    const {addToDoService} = useToDoService();
+    const {user} = userStore();
 
-    const data = {
-      account,  // account : account // key 와 value 의 이름이 같은 경우 값을 하나만 적어도 적용된다. // request dto 의 필드명과 이름이 같아야 한다.
-      password  
-    }
-    const loginHandler = async (e: React.MouseEvent<HTMLButtonElement>) => { // e : 여기서는 onClick 클릭이벤트 타입
-      e.preventDefault(); // 페이지 새로고침 방지
-      userLogin(data); // 로그인 hook(loginAPI + 로그인로직)   
-    }
+   const data: ToDoCreateRequestDTO = {
+    userId: user.id,
+    content: value, // 입력한 값
+    done: false // 기본 값
+  }
 
-
-    const onSubmit = (e: FormEvent) => {
+    const 할일등록 = (e: FormEvent) => {
 			e.preventDefault();
 			if(value.trim() == "") { alert("할 일을 입력해주세요.") }  
       else {
-        addToDo(value);
-        setValue("");
+        addToDoService(data);
+        setValue(''); // 입력값 초기화
       }					 
 		}
 
     return (
       <>
-        <Form noneborder onSubmit={ onSubmit } style={{flex: "1"}}>
+        <Form noneborder onSubmit={ 할일등록 } style={{flex: "1"}}>
 					<Form.Input
             onChange={setValue}
             value={value}
@@ -59,19 +55,6 @@ export default function ToDoInsert({addToDo} : {addToDo: (text: string) => void 
           />
           <button style={{ display: "none" }} />
         </Form>
-
-
-        {/* <form onSubmit={ onSubmit } style={{flex: "1"}}>
-					<Input
-            autoFocus
-            onChange={(e) => { setValue(e.target.value); }}
-            value={value}
-            placeholder="할 일을 입력하고, Enter를 치세요"
-          />
-          <button style={{ display: "none" }} />
-        </form> */}
-      
       </>
-      
     );
 }
