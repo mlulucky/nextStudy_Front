ebae04a2ea2c1210d6be20bd3428a86a33f6425a
mlulucky <dmswjdans@gmail.com>
2023-08-26 23:38:36 +0900

    refactor: 필요없는 파일 및 코드 삭제

--- src/styles/PageWrapper.ts
+++ src/styles/PageWrapper.ts
@@ -1,18 +1,12 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const PageWrapper = styled.div`
+const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
   height: 100vh;
   padding: 1rem;
-`
-
-export const NavWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  box-shadow: 0px 0px 4px 0px #c6c6c6;
-`
+  flex-direction: column;
+`;
+export default PageWrapper;
