76558c0ce6571cca3cf599f70052912c9bdda86c
mlulucky <dmswjdans@gmail.com>
2023-09-04 09:26:55 +0900

    refactor: state props 전달 제거

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -1,10 +1,10 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ToDoInsert from "./TodoInsert";
 import ToDoList from "./ToDoList";
-import { useToDo } from "@/hooks/reducer/useToDo";
 import useToDoService from "@/hooks/useToDoService";
 import ToDoHead from "./ToDoHead";
+import { useCookies } from "react-cookie";
 const ToDoWrap = styled.div`
   border: 1px solid #c6c6c6;
   width: 450px;
@@ -16,17 +16,21 @@ const ToDoWrap = styled.div`
 `;
 
 export default function ToDoTemplate() {
-  const { state } = useToDo();
   const { getToDos } = useToDoService();
-
+	const [cookies, setCookies] = useCookies();
+	const refreshTokenCheck = async () => {
+		const refreshToken = cookies.refreshToken;
+		// 브라우저에 refreshToken 이 있으면 accesstoken 재발급
+	}
   useEffect(() => {
     getToDos();
+		// 로그인체크
   }, []);
-
+	
   return (
     <>
       <ToDoWrap>
-        <ToDoHead state={state} />
+        <ToDoHead/>
         <ToDoInsert/>
         <ToDoList/>
       </ToDoWrap>
