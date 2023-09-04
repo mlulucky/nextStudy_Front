10426e256dc74592fa50ffefc14b5da9fb849ed5
mlulucky <dmswjdans@gmail.com>
2023-09-04 15:55:35 +0900

    refactor: 안쓰는 코드 삭제

--- src/components/ToDoTemplate.tsx
+++ src/components/ToDoTemplate.tsx
@@ -4,7 +4,6 @@ import ToDoInsert from "./TodoInsert";
 import ToDoList from "./ToDoList";
 import useToDoService from "@/hooks/useToDoService";
 import ToDoHead from "./ToDoHead";
-import { useCookies } from "react-cookie";
 const ToDoWrap = styled.div`
   border: 1px solid #c6c6c6;
   width: 450px;
@@ -13,18 +12,16 @@ const ToDoWrap = styled.div`
   padding: 30px;
   display: flex;
   flex-direction: column;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export default function ToDoTemplate() {
   const { getToDos } = useToDoService();
-	const [cookies, setCookies] = useCookies();
-	const refreshTokenCheck = async () => {
-		const refreshToken = cookies.refreshToken;
-		// 브라우저에 refreshToken 이 있으면 accesstoken 재발급
-	}
+
   useEffect(() => {
     getToDos();
-		// 로그인체크
   }, []);
 	
   return (
