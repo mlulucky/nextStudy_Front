07c7f9c3c4926749faafb233dfbc3bfa4ae63336
mlulucky <dmswjdans@gmail.com>
2023-09-04 09:34:25 +0900

    feat: 로그인후 로그인여부 체크하여 로그인시 todolist 페이지로 라우팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,56 @@
+import { useRouter } from "next/router";
+import PageWrapper from "@/styles/PageWrapper";
+import Text from "@/styles/JoinText";
+import LoginForm from "@/components/LoginForm";
+import { useEffect, useState } from "react";
+import useAuth from "@/hooks/useAuth";
+import Loading from "@/components/Loading";
 import userStore from "@/store/userStore";
-import { Login } from "./login";
-import ToDoListPage from "./todolist";
-import Nav from "@/components/Nav";
+import { useCookies } from "react-cookie";
 
-export default function Home() {
-  const { user } = userStore();
+export default function Home() { 
+	const router = useRouter();
+	const {userCheck} = useAuth();
+	const {user} = userStore();
+	const [isLoading, setIsLoading] = useState(true);
+	const [cookies, setCookies] = useCookies();
+	
+	useEffect( ()=>{
+		if(user) { // 로그인 후에 로그인정보가 있는지 확인 후 없으면 login 페이지로 이동
+			userCheck().then(()=>{
+				setIsLoading(false);
+			});
+		} else {
+			setIsLoading(false);
+		}
+	},[])
 
+	if(isLoading) {
+		return (
+			<PageWrapper>
+				<Loading/>
+			</PageWrapper>
+		)
+	} 
+
+	// 로그인 후 "/" url 접근시 todolist 페이지로 이동
+	if (user) {		
+		router.push("/todolist");
+		return null;
+	} 
+	// 브라우저 껏다켜도, 로그인한 상태이면 todolist 페이지로 이동
+	if (cookies.name) {		
+		router.push("/todolist");
+		return null;
+	} 
+	
   return (
-    <>
-      {
-        // 헤더는 로그인 후에 보여지도록 -> 유저가 있으면 헤더 보여주기
-        user && <Nav />
-      }
-      {
-        // 자바스크립트에서 값이 있으면 true, 아니면 false
-        user ? <ToDoListPage /> : <Login />
-      }
-    </>
-  );
+		<>
+			<PageWrapper>
+				<LoginForm/> 
+				<Text onClick={()=>{router.push("/join")}}>아직 회원이 아니신가요? 회원가입</Text>
+			</PageWrapper>
+		</>
+  )
+
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,64 @@
 import PageWrapper from "@/styles/PageWrapper";
 import LoginForm from "@/components/LoginForm";
+import { useRouter } from "next/router";
+import { styled } from "styled-components";
+import { useEffect, useState } from "react";
+import useAuth from "@/hooks/useAuth";
+import userStore from "@/store/userStore";
+import Loading from "@/components/Loading";
+import { useCookies } from "react-cookie";
 
-export function Login(){
-  return (
-    <PageWrapper>
-      <LoginForm/> 
-      <span style={{color: '#777'}}>아직 회원이 아니신가요? 회원가입</span>  
-    </PageWrapper>
-  )
+const Text = styled.span`
+	color: #777;
+	cursor: pointer;
+	&:hover {
+		color: #03c75a;
+	}
+`
+
+export default function Login(){ 
+	const router = useRouter();
+	const {userCheck} = useAuth();
+	const {user} = userStore();
+	const [isLoading, setIsLoading] = useState(true);
+	const [cookies, setCookies] = useCookies();
+	
+	useEffect( ()=>{
+		if(user) { // 로그인 후에 로그인정보가 있는지 확인 후 없으면 login 페이지로 이동
+			userCheck().then(()=>{
+				setIsLoading(false);
+			});
+		} else {
+			setIsLoading(false);
+		}
+	},[])
+
+	if(isLoading) {
+		return (
+			<PageWrapper>
+				<Loading/>
+			</PageWrapper>
+		)
+	} 
+
+	// 로그인 후 "/" url 접근시 todolist 페이지로 이동
+	if (user) {		
+		router.push("/todolist");
+		return null;
+	} 
+
+	// 브라우저 껏다켜도, 로그인한 상태이면 todolist 페이지로 이동
+	if (cookies.name) {		
+		router.push("/todolist");
+		return null;
+	} 
+
+	return (
+		<PageWrapper>
+			<LoginForm/> 
+			<Text onClick={()=>{router.push("/join")}}>아직 회원이 아니신가요? 회원가입</Text>
+		</PageWrapper>
+	)
 }
+
+	
