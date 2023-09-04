e9793b1b59dc74d52a559d7a2ffb5eb36b66f229
mlulucky <dmswjdans@gmail.com>
2023-09-04 09:22:46 +0900

    feat: 날짜  하드코딩에서 newDate 메서드로 설정

--- src/components/ToDoHead.tsx
+++ src/components/ToDoHead.tsx
@@ -1,17 +1,23 @@
 import { ToDoItemType } from "@/hooks/reducer/useToDo";
+import todoStore from "@/store/todoStore";
 import React from "react";
 
-export default function ToDoHead({ state }: { state: ToDoItemType }) {
+export default function ToDoHead() {
   const today = new Date();
   const dateName = today.toLocaleDateString("ko-KR", {
     year: "numeric",
+		month: "long",
+		day: "numeric"
   });
+	const dayName = today.toLocaleDateString("ko-KR", {weekday: "long"});
 
-  const undoneTasks = state.todos.filter((todo) => !todo.done); // to.done 이 false 인 todo 배열
+	const {todos} = todoStore();
+	const undoneTasks = todos.filter((todo)=> !todo.done); // to.done 이 false 인 todo 배열
+
   return (
     <div style={{ textAlign: "left", flex: "1" }}>
-      <h1 style={{ margin: "0px" }}>2019년 8월 7일</h1>
-      <span>수요일</span>
+      <h1 style={{ margin: "0px" }}>{dateName}</h1>
+      <span>{dayName}</span>
       <p>할일 {undoneTasks.length}개</p>
     </div>
   );
